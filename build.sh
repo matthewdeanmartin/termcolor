4ecd396636ecb622d093c1e9e32d47b82e1604f8
Matthew Martin <mmartin@loc.gov>
2021-12-04 15:56:25 -0500

    check that wheel!

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ pylint termcolor
 flake8 termcolor
 python -m pytest termcolor
 python -m pytest --doctest-glob="termcolor/**/*.py"
-
 pytest test -v --cov-report html:coverage --cov=termcolor
+echo not bumping version here, just checking if we can create the wheel
+poetry build
+check-wheel-contents dist/*.whl
